b9e801511f7dda253ecd0e5a7e723ebfe59497ad
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2021-03-14 03:48:17 +0900

    feat: CommandUtils

--- src/main/java/net/kunmc/lab/kikakuutils/utils/CommandUtils.java
+++ src/main/java/net/kunmc/lab/kikakuutils/utils/CommandUtils.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Entity;
 
 import java.util.List;
 
-public class Utils {
+public class CommandUtils {
     public static boolean isValidTarget(CommandSender sender, String text) {
         try {
             List<Entity> tmp = Bukkit.selectEntities(sender, text);
